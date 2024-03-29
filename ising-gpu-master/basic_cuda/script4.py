74148a4c3862f243765e1485e829997ddae98e9d
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-03-29 14:20:59 -0400

    script para rodar processos sequencialmente

---
+++ ising-gpu-master/basic_cuda/script4.py
@@ -0,0 +1,59 @@
+from multiprocessing.pool import ThreadPool
+import subprocess
+
+def f(x):
+    a,b,c = x
+#alpha = [0.285, 0.376, 0.637, 0.8, 1.0]
+alpha = [0.376, 0.637, 0.8, 1.0]
+t1 =  [0.6, 1.2, 1.4, 1.5]
+t2 = [2.5, 2.7, 3.0,3.0]
+
+j = 0
+for i in range (len(alpha)):
+    t = t1[i]
+    t_end = t + 0.02
+    step = 0.002
+    while(t <= t2[i]):
+        l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(alpha[i])+".txt", "10"]
+        subprocess.check_call(l, stdout=subprocess.PIPE)
+        t += 0.02
+        t_end += 0.02
+        j +=1
+
+alpha = [0.285, 0.25, 0.2, 0.15, 0.0]
+t1 =  [0.6, 0.8, 1.0, 1.0, 1.5]
+t2 = [2.4, 2.4, 2.8, 3.0, 3.0]
+
+j = 0
+for i in range (len(alpha)):
+    t = t1[i]
+    t_end = t + 0.02
+    step = 0.002
+    
+    while(t <= t2[i]):
+        l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(alpha[i])+".txt", "10"]
+        subprocess.check_call(l, stdout=subprocess.PIPE)
+        t += 0.02
+        t_end += 0.02
+        j +=1
+
+alpha = [0.285, 0.376, 0.637]
+n = [5, 10, 20]
+t1 =  [0.9, 1.0, 1.5]
+t2 = [2.1, 2.0 , 2.5]
+
+j = 0
+
+for i in range (len(alpha)):
+    for j in range(len(n)):
+        n2 = n[j]
+        al = alpha[i]
+        t = t1[i]
+        t_end = t + 0.02
+        step = 0.002
+        while (t <= t2[i]):
+            l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(n2)+"_"+str(alpha[i])+".txt", str(n2)]
+            subprocess.check_call(l, stdout=subprocess.PIPE)
+            t += 0.02
+            t_end += 0.02
+            j += 1
