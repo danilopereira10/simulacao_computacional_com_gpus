5e66f39b457476581f8c089a5a78cdaa21bb63e6
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-03-29 14:46:12 -0400

    alterando chamada do ising_basic

--- ising-gpu-master/basic_cuda/script4.py
+++ ising-gpu-master/basic_cuda/script4.py
@@ -11,13 +11,10 @@ t2 = [2.5, 2.7, 3.0,3.0]
 j = 0
 for i in range (len(alpha)):
     t = t1[i]
-    t_end = t + 0.02
-    step = 0.002
     while(t <= t2[i]):
-        l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(alpha[i])+".txt", "10"]
+        l = ["./ising_basic", str(alpha[i]), str(t), "0", "0", str(alpha[i])+".txt", "10"]
         subprocess.check_call(l, stdout=subprocess.PIPE)
-        t += 0.02
-        t_end += 0.02
+        t += 0.002
         j +=1
 
 alpha = [0.285, 0.25, 0.2, 0.15, 0.0]
@@ -27,14 +24,12 @@ t2 = [2.4, 2.4, 2.8, 3.0, 3.0]
 j = 0
 for i in range (len(alpha)):
     t = t1[i]
-    t_end = t + 0.02
-    step = 0.002
+
     
     while(t <= t2[i]):
-        l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(alpha[i])+".txt", "10"]
+        l = ["./ising_basic", str(alpha[i]), str(t), "0", "0", str(alpha[i])+".txt", "10"]
         subprocess.check_call(l, stdout=subprocess.PIPE)
-        t += 0.02
-        t_end += 0.02
+        t += 0.002
         j +=1
 
 alpha = [0.285, 0.376, 0.637]
@@ -49,11 +44,8 @@ for i in range (len(alpha)):
         n2 = n[j]
         al = alpha[i]
         t = t1[i]
-        t_end = t + 0.02
-        step = 0.002
         while (t <= t2[i]):
-            l = ["./ising_basic", str(alpha[i]), str(t), str(t_end), str(step), str(n2)+"_"+str(alpha[i])+".txt", str(n2)]
+            l = ["./ising_basic", str(alpha[i]), str(t), "0", "0", str(n2)+"_"+str(alpha[i])+".txt", str(n2)]
             subprocess.check_call(l, stdout=subprocess.PIPE)
-            t += 0.02
-            t_end += 0.02
+            t += 0.002
             j += 1
