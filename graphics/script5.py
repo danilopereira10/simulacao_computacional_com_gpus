4b98bffa0a708ee3ecc1ae87331996931f5b1783
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-06-15 14:46:42 -0400

    Script pro código em cuda

---
+++ graphics/script5.py
@@ -0,0 +1,23 @@
+from multiprocessing.pool import ThreadPool
+import subprocess
+
+def f(x):
+    a,b,c = x
+#alpha = [0.285, 0.376, 0.637, 0.8, 1.0]
+alpha = [0.376, 0.637, 0.8, 1.0]
+t1 =  [0.6, 1.2, 1.4, 1.5]
+t2 = [2.5, 2.7, 3.0,3.0]
+
+j = 0
+nwarmup = "100"
+niters = "1000"
+for i in range (len(alpha)):
+    t = t1[i]
+    t_end = t + 0.02
+    step = 0.0002
+    while(t <= t2[i]):
+        l = ["./ising_basic", str(alpha[i]), str(t), str(alpha[i])+".txt", "177", "12", nwarmup, niters]
+        subprocess.Popen(l, stdout=subprocess.PIPE)
+        t += 0.02
+        t_end += 0.02
+        j +=1
