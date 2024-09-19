f5f3f890a2ef38ad9086a3cac4cfd403dc05a067
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-09-19 15:08:30 -0400

    Script para executar com um valor de alpha e de temperatura

---
+++ graphics/script6.py
@@ -0,0 +1,20 @@
+from multiprocessing.pool import ThreadPool
+import subprocess
+
+def f(x):
+    a,b,c = x
+#alpha = [0.285, 0.376, 0.637, 0.8, 1.0]
+alpha = [1.0]
+t1 =  [2.5]
+t2 = [2.5]
+
+j = 0
+nwarmup = "100"
+niters = "1000"
+for i in range (len(alpha)):
+    t = t1[i]
+    while(t <= t2[i]):
+        l = ["./ising_basic", str(alpha[i]), str(t), str(alpha[i])+"_cu.txt", "177", "12", nwarmup, niters]
+        subprocess.check_call(l, stdout=subprocess.PIPE)
+        t += 0.02
+        j +=1
