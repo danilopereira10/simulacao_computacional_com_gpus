4044096ab2835d65b46b6abcae03ce6995cd4e6d
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-10-04 00:47:59 -0400

    Outro teste

--- ising-gpu-master/basic_cuda/main.cu
+++ ising-gpu-master/basic_cuda/main.cu
@@ -173,20 +173,20 @@ int runc(float alpha, float t, float t_end, float step, char* filename, int N) {
         for (int i = 0; i < N_EQUILIBRIUM+N_AVERAGE; i++) {
             cudaMemcpy(d_matrix, matrix, bytes, cudaMemcpyHostToDevice);
             cudaMemcpy(d_randomMatrix, randomMatrix, bytes2, cudaMemcpyHostToDevice);
-            flip_spins<<<L,N>>>(BLACK, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
-            flip_spins2<<<L,N>>>(BLACK, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins<<<BL,T>>>(BLACK, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins2<<<BL,T>>>(BLACK, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
             cudaDeviceSynchronize();
             reinitialize_random_matrix(N, randomMatrix);
 
             cudaMemcpy(d_randomMatrix, randomMatrix, bytes2, cudaMemcpyHostToDevice);
-            flip_spins<<<L,N>>>(WHITE, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
-            flip_spins2<<<L,N>>>(WHITE, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins<<<BL,T>>>(WHITE, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins2<<<BL,T>>>(WHITE, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
             cudaDeviceSynchronize();
             reinitialize_random_matrix(N, randomMatrix);
 
             cudaMemcpy(d_randomMatrix, randomMatrix, bytes2, cudaMemcpyHostToDevice);
-            flip_spins<<<L,N>>>(GREEN, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
-            flip_spins2<<<L,N>>>(GREEN, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins<<<BL,T>>>(GREEN, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
+            flip_spins2<<<BL,T>>>(GREEN, J0, J1, J2, t, N, d_matrix, d_randomMatrix, L*N);
             cudaDeviceSynchronize();
             reinitialize_random_matrix(N, randomMatrix);
 
