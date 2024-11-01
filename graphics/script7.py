984e2df604d3b1104e4c28d143ee5d1351efcfab
Danilo Pereira da SIlva <danilopereira_b2@hotmail.com.br>
2024-11-01 00:57:56 -0400

    Script pra execução do código em C

---
+++ graphics/script7.py
@@ -0,0 +1,20 @@
+from multiprocessing.pool import ThreadPool
+import subprocess
+
+def f(x):
+    a,b,c = x
+#alpha = [0.285, 0.376, 0.637, 0.8, 1.0]
+alpha = [1.0]
+t1 =  [2.5]
+t2 = [2.5]
+
+j = 0
+
+for i in range (len(alpha)):
+    t = t1[i]
+    while(t <= t2[i]):
+        t_end = t + 0.02
+        l = ["./simulator", str(alpha[i]), str(t), str(t_end), str(1), str(alpha[i])+"_cu.txt", "10"]
+        subprocess.check_call(l, stdout=subprocess.PIPE)
+        t += 0.02
+        j +=1
